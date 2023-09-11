63136810671bae5831cd74f9d22cc15234229c88
Prashanta Das <dasprashanta694@gmail.com>
2023-09-11 20:44:13 +0530

    added a file

---
+++ ass_2.c
@@ -0,0 +1,117 @@
+// //input two numbers and display their addition, subtraction,division,multiplication and remainder result.
+
+// #include<stdio.h>
+// int main()
+// {
+// 	int a,b;
+// 	printf("enter 1st number: ");
+// 	scanf("%d",&a);
+// 	printf("enter 2nd number: ");
+// 	scanf("%d",&b);
+// 	printf("\naddition is: %d",a+b);
+// 	printf("\nsubtraction is: %d",a-b);
+// 	printf("\nmultiplication is: %d",a*b);
+// 	printf("\ndivision is: %d",a/b);
+// }
+
+
+
+
+// //input your name,age,roll no,branch,percentage of mark and display 
+
+// #include<stdio.h>
+// int main()
+// {
+// 	char name[20],branch[20];
+// 	int age, roll_no, mark_perc;
+// 	printf("\nenter name: ");
+// 	scanf("%s",&name);
+// 	printf("\nenter roll number: ");
+// 	scanf("%d",&roll_no);
+// 	printf("\nenter age: ");
+// 	scanf("%d",&age);
+// 	printf("enter branch: ");
+// 	scanf("%s",&branch);
+// 	printf("enter percentage of marks: ");
+// 	scanf("%d",&mark_perc);
+// 	printf("\nyour name is: %s",name);
+// 	printf("\nyour roll number is: %d",roll_no);
+// 	printf("\nyour age is: %d",age);
+// 	printf("\nyour branch is: %s",branch);
+// 	printf("\nyour percentage of marks is: %d",mark_perc);
+// 	return 0;
+// }
+
+
+
+// //Input principal,rate of interest,and time period and calculate simple interest and total amount to pay 
+// #include<stdio.h>
+// int main()
+// {
+// 	float principal,rate_of_interest,time_period;
+// 	printf("enter principal:");
+// 	scanf("%f",&principal);
+// 	printf("enter rate of interest;");
+// 	scanf("%f",&rate_of_interest);
+// 	printf("enter time period:");
+// 	scanf("%f",&time_period);
+// 	float simple_interest=(principal*time_period*rate_of_interest)/100;
+// 	float total_amount=principal+simple_interest;
+// 	printf("simple interest is: %.2f",simple_interest);
+// 	printf("\ntotal amount to pay is: %.2f",total_amount);
+// 	return 0;
+
+// }
+
+
+
+
+
+
+// //Input radius and calculate perimeter and area of circle
+// #include<stdio.h>
+// int main()
+// {
+// 	int pie=22/7;
+// 	int r;
+// 	printf("enter radius: ");
+// 	scanf("%d",&r);
+// 	printf("area of circle is: %d",pie*r*r);
+// 	printf("\nperimeter of circle is: %d",2*pie*r);
+// 	return 0;
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
