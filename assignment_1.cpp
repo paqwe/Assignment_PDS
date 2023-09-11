c014f7c2bec98287823923fcac8e8a3eb7082e2b
Prashanta Das <dasprashanta694@gmail.com>
2023-09-11 20:40:02 +0530

    added a file

---
+++ assignment_1.cpp
@@ -0,0 +1,114 @@
+// //1.Write a program to find the remainder after dividing 314 by 21?
+// #include<stdio.h>
+// int main() 
+// {
+// 	printf("remainder after dividing is %d", 314%21);
+// }
+
+
+// //2.Predict the output (Celsius to Fahrenheit conversion)
+
+// #include<stdio.h>
+// int main()
+// {
+// 	float c=5.0;
+// 	printf("temperature in fahrenheit is %.2f",(9/5.0)*c+32);
+
+// 	return 0;
+// }
+
+
+// // 3.Predict the output of following C program (octal to decimal, binary to decimal,
+// // hexadecimal to decimal).
+
+// #include<stdio.h>
+// int main()
+// {
+// 	int a=012;
+// 	int b=0b101;
+// 	int c=0xF;
+// 	printf("octal to decimal conversion: %d\n",a);
+// 	printf("binary to decimal conversion: %d\n",b);
+// 	printf("hexadecimal to decimal conversion: %d\n",c);
+// 	return 0;
+// }
+
+
+// //4.What is the output of following code(using white space).
+
+// #include<stdio.h>
+// int main()
+// {
+// int num = - -2;
+// printf("num = %d", num);
+// return 0;
+// }
+
+
+
+// //5.What will be the output of following C program.
+
+// #include<stdio.h>
+// int main()
+// {
+// float a = 5.0;
+// printf ("Result is = %d ", (24 / 5) * a);
+// return 0;
+// }
+
+
+
+// //6.write a C program to print Integer equivalent of character data types.
+
+// #include<stdio.h>
+// int main()
+// {
+// 	char a='a';
+// 	char c;
+// 	printf("value of a: %c\n",a);
+// 	a++;
+// 	printf("value of a after increment is: %c\n",a);
+// 	c=99;
+// 	printf("Value of c: %c", c);
+// 	return 0;
+
+// }
+
+
+
+
+// //7.Program to know the size of data type in memory.
+
+// #include <stdio.h>
+// int main()
+// {
+// printf("sizeof(char) => %d\n", sizeof(char));
+// printf("sizeof(short) => %d\n", sizeof(short));
+// printf("sizeof(short int) => %d\n", sizeof(short int));
+// printf("sizeof(int) => %d\n", sizeof(int));
+// printf("sizeof(long) => %d\n", sizeof(long));
+// printf("sizeof(long int) => %d\n", sizeof(long int));
+// printf("sizeof(long long) => %d\n", sizeof(long long));
+// printf("sizeof(long long int) => %d\n", sizeof(long long int));
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
