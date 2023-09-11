f4ce06a8b410b135e99b7e4442a37237caf37fbd
Prashanta Das <dasprashanta694@gmail.com>
2023-09-11 20:47:49 +0530

    added a file

---
+++ ass_5.c
@@ -0,0 +1,105 @@
+//write a program to find sum of digits of a number using for loop.
+// #include<stdio.h>
+// int main()
+// {
+// 	int sum,num;
+// 	printf("enter a number: ");
+// 	scanf("%d",&num);
+// 	for(sum=0; num>0; num=num/10)
+// 		sum+=num%10;
+// 	printf("the sum of the digits is: %d",sum);
+
+
+// }
+
+
+// //write a program to count number of digits in number using for loop.
+// #include<stdio.h>
+// int main()
+// {
+// 	int rem,num,count;
+// 	printf("enter a number: ");
+// 	scanf("%d",&num);
+// 	for(count=0; num>0; num=num/10)
+// 	{
+// 		rem+=num%10;
+// 		count=count+1;
+
+// 	}
+// printf("the number of the digits is: %d",count);
+
+
+// }
+
+
+
+//write a program to reverse a number using for loop.
+
+
+
+// //write a program to enter a number and check whether it is a prime number or not using a for loop in c.
+// #include<stdio.h>
+// int main()
+// {
+// 	int i,n,count=0;
+// 	printf("enter a number: ");
+// 	scanf("%d",&n);
+// 	for(i=2; i<n; i++)
+// 	{
+// 		if(n%i==0)
+// 			count++;
+// 	}
+// 	if(count>0)
+// 		printf("%d is not a prime number",n);
+// 	else
+// 		printf("%d is a prime number",n);
+// }
+
+
+
+// //print numbers from 20 to 50 using for loop
+// #include<stdio.h>
+// int main()
+// {
+// 	for(int i=20; i<=50; i++)
+// 		printf("\n%d",i);
+// }
+
+
+
+//print numbers from 50 to 1 using for loop.
+#include<stdio.h>
+int main()
+{
+	for(int i=50; i>=1; i--)
+		printf("\n%d",i);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
