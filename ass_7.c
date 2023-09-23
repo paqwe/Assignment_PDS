a966946d6f3c445dcd1526c51edc7b77e21ad76c
prashanta das <106626017+paqwe@users.noreply.github.com>
2023-09-23 20:13:45 +0530

    Add files via upload

---
+++ ass_7.c
@@ -0,0 +1,169 @@
+////A
+//#include<stdio.h>
+//int main()
+//{
+//	int i;
+//	for(i=1; i<=25; i++)
+//	{
+//		printf("%d ",i);
+//		if(i%5==0)
+//		{
+//			printf("\n");
+//		}
+//		
+//	}
+//}
+
+
+////B
+//#include<stdio.h>
+//int main()
+//{
+//	int i,j;
+//	for(i=1; i<=5; i++)
+//	{
+//		for(j=1; j<=i; j++)
+//		{
+//			printf("%d",j);
+//		}
+//		printf("\n");
+//	}
+//}
+
+
+////c
+//#include<stdio.h>
+//int main()
+//{
+//	int i,j;
+//	for(i=1; i<=5; i++)
+//	{
+//		for(j=1; j<=i; j++)
+//		{
+//			printf("%d",i);
+//		}
+//		printf("\n");
+//	}
+//}
+
+
+
+
+
+////d
+//#include<stdio.h>
+//int main()
+//{
+//	int rows=5,i,j,k;
+//	for(i=1; i<=rows; i++)
+//	{
+//		for(j=1; j<=rows-i; j++)
+//		{
+//			printf(" ");
+//		}
+//		for(k=1; k<=i; k++)
+//		{
+//			printf("*");
+//		}
+//		printf("\n");
+//	}
+//	return 0;
+//}
+
+
+//e
+//#include<stdio.h>
+//int main()
+//{
+//	int i,j;
+//	for(i=1; i<=5; i++)
+//	{
+//		for(j=5; j>=i; j--)
+//		{
+//			printf("*");
+//		}
+//		printf("\n");
+//	}
+//}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//d
+//#include<stdio.h>
+//int main()
+//{
+//	int rows=5,i,j,k;
+//	for(i=1; i<=rows; i++)
+//	{
+//		for(j=1; j<=rows-i; j++)
+//		{
+//			printf(" ");
+//		}
+//		for(k=1; k<=i; k++)
+//		{
+//			printf("*");
+//		}
+//		printf("\n");
+//	}
+//	return 0;
+//}
+
+//f
+//#include<stdio.h>
+//int main()
+//{
+//	
+//}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
